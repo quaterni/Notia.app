d99fc2c02c823ae648b48cad67191d2361e2d6dc
Igor Zhadko <igorzhadko.w@gmail.com>
2024-10-28 00:02:33 +0500

    Add email to user model

--- Np.UsersService/Np.UsersService.Core/Models/Users/User.cs
+++ Np.UsersService/Np.UsersService.Core/Models/Users/User.cs
@@ -19,6 +19,8 @@ public class User
     [MinLength(3)]
     public required string Username { get; set; }
 
+    public required string Email { get; set; }
+
     public required Guid Id { get; init; }
 
     public required string IdentityId { get; init; }
