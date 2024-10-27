9a7738c0f3acd566afa78ef035460f2f005e551e
Igor Zhadko <igorzhadko.w@gmail.com>
2024-10-27 19:39:44 +0500

    Create EF Core DbContext
    - Add user configuration
    - Add ApplicationDbContext
    - Add migration
    - Add DbContext to DI
    - Add empty connection string in appsettings.Empty.json

---
+++ Np.UsersService/Np.UsersService.Core/Data/Configurations/UserConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Np.UsersService.Core.Models.Users;
+
+namespace Np.UsersService.Core.Data.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.ToTable("users");
+
+        builder.HasKey(u => u.Id);
+        builder.HasAlternateKey(u => u.IdentityId);
+
+        builder.HasIndex(u=> u.Username).IsUnique();
+
+        builder.Property(u=> u.Username).IsRequired();
+        builder.Property(u=> u.Id);
+        builder.Property(u=> u.IdentityId);
+    }
+}
