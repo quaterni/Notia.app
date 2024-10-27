f17cc0215a118eacd10375c784281f75f9c326f6
Igor Zhadko <igorzhadko.w@gmail.com>
2024-10-28 00:02:33 +0500

    Add email to user model

---
+++ Np.UsersService/Np.UsersService.Core/Data/Migrations/20241027190058_AddUserEmail.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Np.UsersService.Core.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserEmail : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_Email",
+                table: "users",
+                column: "Email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_users_Email",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "users");
+        }
+    }
+}
