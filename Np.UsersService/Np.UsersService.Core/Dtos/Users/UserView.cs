7623bf1b03035acebe3c83eeb799ef5f9a0c7145
Igor Zhadko <igorzhadko.w@gmail.com>
2024-10-29 08:50:08 +0500

    Add GetUserAsync in identity service

---
+++ Np.UsersService/Np.UsersService.Core/Dtos/Users/UserView.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Np.UsersService.Core.Dtos.Users;
+
+public class UserView
+{
+    public string Username { get; set; }
+
+    public string Email { get; set; }
+
+    [JsonPropertyName("id")]
+    public string IdentityId { get; set; }
+}
