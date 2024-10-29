86613879019ac092177aa1262bcbd8e1f83f3fee
Igor Zhadko <igorzhadko.w@gmail.com>
2024-10-29 22:34:07 +0500

    Create keycloak token service
    - Add ITokenService interface
    - Add AuthorizationToken model with access and refresh token
    - Add UserCredentials that used by ITokenService
    - Add AuthCientOptions that used by KeycloakTokenService
    - Add KeycloakTokenService
    - Update DepencencyInjection

---
+++ Np.UsersService/Np.UsersService.Core/Authentication/Keycloak/Options/AuthClientOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace Np.UsersService.Core.Authentication.Keycloak.Options;
+
+public class AuthClientOptions
+{
+    public string ClientId { get; init; } = string.Empty;
+
+    public string ClientSecret { get; init; } = string.Empty;
+
+    public string TokenUrl { get; init; } = string.Empty;
+}
