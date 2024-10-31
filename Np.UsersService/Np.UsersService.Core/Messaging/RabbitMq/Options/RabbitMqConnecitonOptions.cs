818eefda91db3b90dc4abd9c374f5a4a91b3d6bc
Igor Zhadko <igorzhadko.w@gmail.com>
2024-10-31 10:20:26 +0500

    Create rebbitmq channel factory

---
+++ Np.UsersService/Np.UsersService.Core/Messaging/RabbitMq/Options/RabbitMqConnecitonOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Np.UsersService.Core.Messaging.RabbitMq.Options;
+
+public class RabbitMqConnecitonOptions
+{
+    public string Host { get; set; } = string.Empty;
+    public int Port { get; set; }
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
