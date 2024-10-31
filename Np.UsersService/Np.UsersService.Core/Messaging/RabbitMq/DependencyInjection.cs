53cc1f9509fcad670e0ca31a5097db9d2631d2b1
Igor Zhadko <igorzhadko.w@gmail.com>
2024-11-01 01:53:25 +0500

    Add rabbitmq dependency injection extension

---
+++ Np.UsersService/Np.UsersService.Core/Messaging/RabbitMq/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Np.UsersService.Core.Messaging.RabbitMq.Options;
+
+namespace Np.UsersService.Core.Messaging.RabbitMq;
+
+public static class DependencyInjection
+{
+    public static void AddRabbitMqMessaging(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<RabbitMqConnecitonOptions>(configuration.GetSection("RabbitMq:ConnectionOptions"));
+        services.Configure<RabbitMqExchangeOptions>(configuration.GetSection("RabbitMq:ExchangeOptions"));
+
+        services.AddSingleton<RabbitMqChannelFactory>();
+    }
+}
