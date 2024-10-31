4e6aa8ff09e02896d0ac6967677bcf19b4948592
Igor Zhadko <igorzhadko.w@gmail.com>
2024-11-01 01:52:41 +0500

    Create outbox worker
    - Add outbox entry
    - Add outbox options
    - Add outbox worker
    - Add migrate ef core with outbox entry
    - Add dependency injection
    - Update log level of entity framework database commands

---
+++ Np.UsersService/Np.UsersService.Core/Messaging/Outbox/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Np.UsersService.Core.Messaging.Outbox.Options;
+
+namespace Np.UsersService.Core.Messaging.Outbox;
+
+public static class DependencyInjection
+{
+    public static void AddOutboxEntries(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<OutboxOptions>(configuration.GetSection("OutboxOptions"));
+        services.AddHostedService<OutboxWorker>();
+    }
+}
