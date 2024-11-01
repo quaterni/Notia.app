24e4bd45d6b316e87a592706f1593f4c9dd9883e
Igor Zhadko <igorzhadko.w@gmail.com>
2024-11-01 23:43:55 +0500

    Create Rabbit MQ queue handler
    - Add RabbitMqQueueWorker
    - Add queue options
    - Add message handler
    - Add IApplicationEvent
    - Include MediatR

---
+++ Np.UsersService/Np.UsersService.Core/Messaging/RabbitMq/Options/RabbitMqQueueOptions.cs
@@ -0,0 +1,14 @@
+﻿namespace Np.UsersService.Core.Messaging.RabbitMq.Options;
+
+public class RabbitMqQueueOptions
+{
+    public string QueueName { get; set; } = string.Empty;
+
+    public bool Durable { get; set; }
+
+    public bool AutoDelete { get; set; }
+
+    public bool Exclusive { get; set; }
+
+    public string ExchangeName { get; set; } = string.Empty;
+}
