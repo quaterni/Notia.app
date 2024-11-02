511e6e166e06aa553c26c116bada3d0f00489768
Igor Zhadko <igorzhadko.w@gmail.com>
2024-11-03 01:11:08 +0500

    Implement result pattern
    - Add Result class
    - Add Error model

---
+++ Np.UsersService/Np.UsersService.Core/Shared/Error.cs
@@ -0,0 +1,6 @@
+﻿namespace Np.UsersService.Core.Shared;
+
+public record Error(string Name, string Description)
+{
+    public static Error NullError => new("NullError", "Application return null.");
+}
