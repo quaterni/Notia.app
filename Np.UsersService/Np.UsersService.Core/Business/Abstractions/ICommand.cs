da0c2791073275fd9103aa62e44b809a1eea174e
Igor Zhadko <igorzhadko.w@gmail.com>
2024-11-03 02:42:53 +0500

    Create CQRS pipeline with MediatR

---
+++ Np.UsersService/Np.UsersService.Core/Business/Abstractions/ICommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Np.UsersService.Core.Shared;
+
+namespace Np.UsersService.Core.Business.Abstractions;
+
+public interface ICommand : IRequest<Result>, ICommandBase;
+
+public interface ICommand<TValue> : IRequest<Result<TValue>>, ICommandBase;
+
+public interface ICommandBase : IBaseRequest;
