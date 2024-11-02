2566ff36c568c2c4b103b29cbf93270bce065e48
Igor Zhadko <igorzhadko.w@gmail.com>
2024-11-03 04:31:52 +0500

    Register message handling

---
+++ Np.UsersService/Np.UsersService.Core/Messaging/MessageHandling/DependencyInjection.cs
@@ -0,0 +1,9 @@
+﻿namespace Np.UsersService.Core.Messaging.MessageHandling;
+
+public static class DependencyInjection
+{
+    public static void AddMessageHandling(this IServiceCollection services)
+    {
+        services.AddScoped<MessageHandler>();
+    }
+}
