fd232a595bcc536f65db8beb452163dd83c77962
Igor Zhadko <igorzhadko.w@gmail.com>
2024-11-03 02:42:53 +0500

    Create CQRS pipeline with MediatR

---
+++ Np.UsersService/Np.UsersService.Core/Business/Abstractions/IQuery.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using Np.UsersService.Core.Shared;
+
+namespace Np.UsersService.Core.Business.Abstractions;
+
+public interface IQuery<TValue> : IRequest<Result<TValue>>;
---
+++ Np.UsersService/Np.UsersService.Core/Business/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Np.UsersService.Core.Business.Behaviors;
+
+namespace Np.UsersService.Core.Business;
+
+public static class DependencyInjection
+{
+    public static void AddBusiness(this IServiceCollection services)
+    {
+        services.AddMediatR(opt =>
+        {
+            opt.RegisterServicesFromAssemblyContaining<Program>();
+        });
+    }
+}
