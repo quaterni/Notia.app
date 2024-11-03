59c28177460fd263041dc19f152fc30519be4a18
Igor Zhadko <igorzhadko.w@gmail.com>
2024-11-03 21:00:36 +0500

    Create application event handler to UserCreatedSecurityEvent
    - Update identity service return result pattern to CreateUserAsync and add GetUserByCredentialsAsync method
    - Add IdentityErrors
    - Update user model

---
+++ Np.UsersService/Np.UsersService.Core/Data/Migrations/20241102222956_NullabeIdentityId_SyncFlag.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Np.UsersService.Core.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class NullabeIdentityId_SyncFlag : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropUniqueConstraint(
+                name: "AK_users_IdentityId",
+                table: "users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "IdentityId",
+                table: "users",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSyncrhonizedWithIdentity",
+                table: "users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_IdentityId",
+                table: "users",
+                column: "IdentityId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_users_IdentityId",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "IsSyncrhonizedWithIdentity",
+                table: "users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "IdentityId",
+                table: "users",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AddUniqueConstraint(
+                name: "AK_users_IdentityId",
+                table: "users",
+                column: "IdentityId");
+        }
+    }
+}
--- Np.UsersService/Np.UsersService.Core/Messaging/MessageHandling/MessageHandler.cs
+++ Np.UsersService/Np.UsersService.Core/Messaging/MessageHandling/MessageHandler.cs
@@ -1,5 +1,7 @@
 ﻿using MediatR;
 using Newtonsoft.Json;
+using Np.UsersService.Core.Messaging.MessageHandling.Users.UserCreatedSecure;
+using Np.UsersService.Core.Messaging.ModelEvents.Users;
 using Np.UsersService.Core.Messaging.Models;
 
 namespace Np.UsersService.Core.Messaging.MessageHandling;
@@ -7,21 +9,40 @@ namespace Np.UsersService.Core.Messaging.MessageHandling;
 public partial class MessageHandler
 {
     private readonly ILogger<MessageHandler> _logger;
+    private readonly IPublisher _publisher;
 
     public MessageHandler(
         ILogger<MessageHandler> logger,
         IPublisher publisher)
     {
         _logger = logger;
+        _publisher = publisher;
     }
 
     public async Task Handle(MessageBusEvent message)
     {
+        switch (message.EventName)
+        {
+            case nameof(UserCreatedSecureEvent):
+                var e = DeserializeEvent<UserCreatedSecureApplicationEvent>(message.Body);
+                LogPublishEvent(_logger, e.GetType().Name); ;
+                await _publisher.Publish(e);
+                break;
+            default:
+                LogUnknownEvent(_logger, message.EventName);
+                break;
+        }
         return;
     }
 
     [LoggerMessage(Level=LogLevel.Information, Message="Event deserialized: {EventName}")]
     private static partial void LogEventDeserialized(ILogger logger, string eventName);
+
+    [LoggerMessage(Level = LogLevel.Information, Message = "Message handler publish event: {EventName}")]
+    private static partial void LogPublishEvent(ILogger logger, string eventName);
+
+    [LoggerMessage(Level = LogLevel.Information, Message = "Message handler doesn't have handler to event: {EventName}")]
+    private static partial void LogUnknownEvent(ILogger logger, string eventName);
 
     private TEvent DeserializeEvent<TEvent>(string data) where TEvent : IApplicationEvent
     {
