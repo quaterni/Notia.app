718d96ab45eeacbb549a7ee1882a5ecfb8ce8449
Igor Zhadko <igorzhadko.w@gmail.com>
2024-11-07 10:26:24 +0500

    Create register user command

---
+++ Np.UsersService/Np.UsersService.Core/Business/Commands/RegisterUser/RegisterUserCommand.cs
@@ -0,0 +1,8 @@
+﻿using Np.UsersService.Core.Business.Abstractions;
+
+namespace Np.UsersService.Core.Business.Commands.RegisterUser;
+
+public sealed record RegisterUserCommand(
+    string Username, 
+    string Email, 
+    string Password) : ICommand;
