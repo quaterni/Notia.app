bf1de1beb72212f96b651e1ffdb7e566445b156d
Igor Zhadko <igorzhadko.w@gmail.com>
2024-11-07 10:26:24 +0500

    Create register user command

---
+++ Np.UsersService/Np.UsersService.Core/Business/Commands/RegisterUser/RegisterUserCommandHandler.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Np.UsersService.Core.Business.Abstractions;
+using Np.UsersService.Core.Data;
+using Np.UsersService.Core.Messaging.ModelEvents.Abstractions;
+using Np.UsersService.Core.Messaging.ModelEvents.Users;
+using Np.UsersService.Core.Models.Users;
+using Np.UsersService.Core.Shared;
+
+namespace Np.UsersService.Core.Business.Commands.RegisterUser;
+
+public class RegisterUserCommandHandler : ICommandHandler<RegisterUserCommand>
+{
+    private readonly ApplicationDbContext _dbContext;
+    private readonly IModelEventService _modelEventService;
+
+    public RegisterUserCommandHandler(
+        ApplicationDbContext dbContext,
+        IModelEventService modelEventService)
+    {
+        _dbContext = dbContext;
+        _modelEventService = modelEventService;
+    }
+
+    public async Task<Result> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
+    {
+        if(await ContainsEmail(request.Email, cancellationToken))
+        {
+            return Result.Failure(RegisterUserErrors.EmailExists);
+        }
+
+        if (await ContainsUsername(request.Username, cancellationToken))
+        {
+            return Result.Failure(RegisterUserErrors.UsernameExists);
+        }
+
+        var user = new User(request.Username, request.Email, Guid.NewGuid());
+
+        _dbContext.Add(user);
+
+        _modelEventService.PublishEvent(new UserCreatedEvent(user.Id));
+        _modelEventService.PublishEvent(
+            new UserCreatedSecureEvent(
+                user.Username, 
+                user.Email, 
+                request.Password, 
+                user.Id));
+
+        await _dbContext.SaveChangesAsync();
+
+        return Result.Success();
+    }
+
+    private async Task<bool> ContainsUsername(string username, CancellationToken cancellationToken)
+    {
+        return await _dbContext.Set<User>()
+            .Where(u => u.Username == username)
+            .AnyAsync(cancellationToken);
+    }
+
+    private async Task<bool> ContainsEmail(string email, CancellationToken cancellationToken)
+    {
+        return await _dbContext.Set<User>()
+            .Where(u => u.Email == email)
+            .AnyAsync(cancellationToken);
+    }
+}
---
+++ Np.UsersService/Np.UsersService.Core/Business/Commands/RegisterUser/RegisterUserErrors.cs
@@ -0,0 +1,11 @@
+﻿
+using Np.UsersService.Core.Shared;
+
+namespace Np.UsersService.Core.Business.Commands.RegisterUser;
+
+public class RegisterUserErrors
+{
+    public static Error EmailExists => new("RegisterUser:EmailExists", "Email was taken by another user");
+
+    public static Error UsernameExists => new("RegisterUser:UsernameExists", "Username was taken by another user");
+}
