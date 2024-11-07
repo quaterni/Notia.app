d75d7c947f1a3574c2da5af1fa9af3d7738e1d8a
Igor Zhadko <igorzhadko.w@gmail.com>
2024-11-07 10:21:12 +0500

    Add UnhandlerdErrorException

---
+++ Np.UsersService/Np.UsersService.Core/Exceptions/UnhandledErrorException.cs
@@ -0,0 +1,25 @@
+﻿using Np.UsersService.Core.Shared;
+
+namespace Np.UsersService.Core.Exceptions;
+
+public class UnhandledErrorException : Exception
+{
+    private const string DefaultMessage = "Error was unhandled {0}";
+
+    public UnhandledErrorException(Error error) : base(string.Format(DefaultMessage, error.Name))
+    {
+        Error = error;
+    }
+
+    public UnhandledErrorException(Error error, string? message) : base(message)
+    {
+        Error = error;
+    }
+
+    public UnhandledErrorException(Error error, string? message, Exception? innerException) : base(message, innerException)
+    {
+        Error = error;
+    }
+
+    public Error Error { get; }
+}
