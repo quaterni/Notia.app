d7162aef0e55e1a9b2513e06d11d0d757e88356e
Igor Zhadko <igorzhadko.w@gmail.com>
2024-11-23 03:07:02 +0500

    Create result pattern and error model

---
+++ Desctop/Notia.Desctop/Services/Abstractions/Error.cs
@@ -0,0 +1,7 @@
+﻿
+namespace Notia.Desctop.Services.Abstractions;
+
+public record Error(string Name, string Description)
+{
+    public static Error NullValue => new("NullValue", "Value was null");
+}
