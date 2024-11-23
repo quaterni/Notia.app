82805bb27a61ca75954230b568fb5a3c0b2e90e6
Igor Zhadko <igorzhadko.w@gmail.com>
2024-11-24 01:45:25 +0500

    Subscribe MainViewModel to changing page of AppRouter

--- Desctop/Notia.Desctop/ViewModels/MainViewModel.cs
+++ Desctop/Notia.Desctop/ViewModels/MainViewModel.cs
@@ -1,17 +1,37 @@
 ﻿using Notia.Desctop.Routing;
+using ReactiveUI;
+using System;
 
 namespace Notia.Desctop.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
     private readonly AppRouter _router;
+    private ViewModelBase _currentViewModel;
+    private readonly IDisposable _routerChangePageSubscription;
 
     public MainViewModel(AppRouter router)
     {
         _router = router;
-        CurrentViewModel = router.CurrentPage;
+        _currentViewModel = router.CurrentPageViewModel;
+
+        _routerChangePageSubscription =  router
+            .WhenAnyValue(r=> r.CurrentPageViewModel)
+            .Subscribe(newPageViewModel => 
+            { 
+                CurrentViewModel = newPageViewModel; 
+            });
     }
 
-    public ViewModelBase CurrentViewModel { get; private set; }
+    public ViewModelBase CurrentViewModel
+    {
+        get => _currentViewModel;
+        private set => this.RaiseAndSetIfChanged(ref _currentViewModel, value);
+    }
 
+    public override void Dispose()
+    {
+        _routerChangePageSubscription.Dispose();
+        base.Dispose();
+    }
 }
