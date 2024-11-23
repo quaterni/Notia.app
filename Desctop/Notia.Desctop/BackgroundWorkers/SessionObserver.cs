4edd291fb77787797f3c98c4297aafe644576d6a
Igor Zhadko <igorzhadko.w@gmail.com>
2024-11-24 01:48:35 +0500

    Create SessionObserver

---
+++ Desctop/Notia.Desctop/BackgroundWorkers/SessionObserver.cs
@@ -0,0 +1,55 @@
+﻿
+using Microsoft.Extensions.Hosting;
+using Notia.Desctop.Routing;
+using Notia.Desctop.Services.Sessions.Abstractions;
+using Notia.Desctop.Stores;
+using Notia.Desctop.ViewModels.Pages;
+using ReactiveUI;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Notia.Desctop.BackgroundWorkers;
+
+internal class SessionObserver : BackgroundService
+{
+    private readonly AppRouter _appRouter;
+    private readonly SessionStore _sessionStore;
+    private IDisposable? _observableSubscription;
+
+    public SessionObserver(
+        AppRouter appRouter,
+        SessionStore sessionStore)
+    {
+        _appRouter = appRouter;
+        _sessionStore = sessionStore;
+    }
+
+    public override Task StartAsync(CancellationToken cancellationToken)
+    {
+        _observableSubscription = _sessionStore
+            .WhenAnyValue(s => s.CurrentSession)
+            .Subscribe(MoveToLoginIfNull);
+        return base.StartAsync(cancellationToken);
+    }
+
+    public override Task StopAsync(CancellationToken cancellationToken)
+    {
+        _observableSubscription?.Dispose();
+
+        return base.StopAsync(cancellationToken);
+    }
+
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    private void MoveToLoginIfNull(Session? session)
+    {
+        if (session == null)
+        {
+            _appRouter.Navigate<LoginPageViewModel>();
+        }
+    }
+}
