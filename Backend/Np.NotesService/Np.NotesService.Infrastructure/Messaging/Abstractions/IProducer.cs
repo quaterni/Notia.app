60c9455811c37b8f400865fa9996b069b40ef267
Igor Zhadko <igorzhadko.w@gmail.com>
2024-12-24 15:39:28 +0500

    add IProducer to notes service

---
+++ Backend/Np.NotesService/Np.NotesService.Infrastructure/Messaging/Abstractions/IProducer.cs
@@ -0,0 +1,7 @@
+﻿
+namespace Np.NotesService.Infrastructure.Messaging.Abstractions;
+
+public interface IProducer<in TMessage> : IDisposable
+{
+    Task SendAsync(TMessage message, CancellationToken cancellationToken = default);
+}
